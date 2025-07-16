62b7deb05d76ea9504c721dec9eb7427c2201043
Szobika410 <jitaruszabolcs@gmail.com>
2025-07-16 15:43:44 +0300

    Second commit

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -4,6 +4,8 @@ import {headers} from "next/headers";
 import {NextResponse} from "next/server";
 import {Webhook} from "svix";
 
+import {createUser} from "@/lib/actions/user.action";
+
 export async function POST(req: NextRequest) {
     try {
       const evt = await verifyWebhook(req)
@@ -12,6 +14,35 @@ export async function POST(req: NextRequest) {
       // For this guide, log payload to console
       const { id } = evt.data
       const eventType = evt.type
+
+      if (eventType === "user.created") {
+        const {id, name, email_addresses, image_url, first_name, last_name} = evt.data
+
+        const user = {
+          clerkId: id,
+          name: name!,
+          email: email_addresses[0].email_address,
+          imageUrl: image_url,
+          firstName: first_name,
+          lastName: last_name,
+        }
+
+        console.log(user);
+
+        const newUser = await createUser(user);
+        
+        if(newUser){
+            await clerkClient().users.updateUser(id, {
+               publicMetadata: {
+                userId: newUser._id,
+               },
+            });
+        }
+
+        return NextResponse.json ({message: "User created", user: newUser});
+
+
+      }
       console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
       console.log('Webhook payload:', evt.data)
   
